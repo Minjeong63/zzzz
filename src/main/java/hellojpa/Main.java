f032c352790ad7080cadbe16d1f1b00a17cf4c4e
Minjeong63 <kmj06030@naver.com>
2024-01-18 15:12:05 +0900

    hello

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("hello ~~~~~~~");
+
         System.out.println("Hello world!");
     }
 }
